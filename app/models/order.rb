8c86cd5492cbd5e8bbe04f8d2ce0c2f6145d5bbb
Kunj Dedhia <kdedhia@umass.edu>
2019-04-07 22:00:26 -0400

    update order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,19 @@
-class Order < ApplicationRecord
+class Order < ActiveRecord::Base
   belongs_to :order_status
+  has_many :order_items
+  before_create :set_order_status
+
+  def subtotal
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+  end
+
+
+private
+
+  def set_order_status
+  end
+
+  def update_subtotal
+  end
+
 end
