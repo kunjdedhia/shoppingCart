a846944a8b4a073bde7058f612aca9e37b0133e9
Kunj Dedhia <kdedhia@umass.edu>
2019-04-07 22:05:31 -0400

    update order_status model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,31 @@
-class OrderItem < ApplicationRecord
+class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :order_present
+
+  before_save :finalize
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+private
+
+  def order_present
+    if order.nil?
+      errors.add(:order, "is not a valid order.")
+    end
+  end
+
+  end
 end
